46765792aee35ba6f4e8dbeceb6b2e45b48ff7bb
Benny Lerner <benjaminlerner@hotmail.com>
2023-04-02 15:50:11 -0400

    test_min_greater_than passes

--- BinarySearchTree_tests.cpp
+++ BinarySearchTree_tests.cpp
@@ -119,15 +119,15 @@ TEST(test_traverse_order_impl_basic) {
     bst.traverse_preorder(std::cout);
     std::cout << std::endl;
 }    
-/*
+
 TEST(test_min_greater_impl_basic) {
     BinarySearchTree<int> bst;
     for (int i = 0; i < 20; ++i) {
         bst.insert(i);
     }
     auto i1 = bst.begin();
-    ASSERT_EQUAL(i1, bst.min_greater_than(19));
     ++i1;
+    ASSERT_EQUAL(i1, bst.min_greater_than(0));
     ++i1;
     ++i1;
     ++i1;
@@ -142,5 +142,5 @@ TEST(test_min_greater_impl_basic) {
     ++i1;
     ASSERT_EQUAL(i1, bst.min_greater_than(10));
 }
-*/
+
 TEST_MAIN()
