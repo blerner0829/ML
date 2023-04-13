c74de713d8fafce4145ddae921ea2e1327f1ac4d
Josh Zirin <jrzirin@umich.edu>
2023-04-13 16:10:35 -0400

    private members of classifier and beginning of public

--- main.cpp
+++ main.cpp
@@ -7,9 +7,31 @@
 #include <sstream>
 #include <cmath>
 #include <set>
-#include <unordered_map>
+#include <map>
+#include "csvstream.h"
 
 using namespace std;
+
+class Classifier {
+  private:
+    int numPosts = 0;
+    set<string> unique_words;
+    map<string, int> word_occur;
+    map<string, int> label_occur;
+    map<string, map<string, int>> label_word_counts;
+    map<string, map<string, string>> string_storage;
+
+  public:
+    map<string, map<string, string>> storeString(csvstream file) {
+      map<string, string> row;
+      while (file >> row){
+        string_storage[row["n"]][row["tag"]] = row["content"];
+      }
+      return string_storage;
+    }
+    
+
+};
 
 set<string> unique_words(const string &str) {
   istringstream source(str);
@@ -22,22 +44,14 @@ set<string> unique_words(const string &str) {
 }
 
 int main(int argc, char* argv[]) {
+  cout.precision(3);
   if ((argc != 3) && (argc != 4) || ((argc == 4) && (argv[3] != "--debug"))) {
     cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << endl;
     return 1;
   };
 
-  ifstream trainFile(argv[1]);
-  if (!trainFile.is_open()) {
-    cout << "Error opening file: " << argv[1] << endl;
-    return 1;
-  }
+  csvstream trainFile(argv[1]);
+  csvstream testFile(argv[2]);
 
-  ifstream testFile(argv[2]);
-  if (!testFile.is_open()) {
-    cout << "Error opening file: " << argv[2] << endl;
-    return 1;
-  }
 
-  
 }
