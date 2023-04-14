785797bb6d3936bc1fd1c57f3f08753f90212b84
Josh Zirin <jrzirin@umich.edu>
2023-04-13 20:34:01 -0400

    one more trainer function left

--- main.cpp
+++ main.cpp
@@ -15,7 +15,8 @@ using namespace std;
 class Classifier {
   private:
     int numPosts = 0;
-    set<string> unique_words;
+    int total_unique_words = 0;
+    set<string> unique_word_set;
     map<string, int> word_occur;
     map<string, int> label_occur;
     map<string, map<string, int>> label_word_counts;
@@ -29,8 +30,64 @@ class Classifier {
       }
       return string_storage;
     }
-    
 
+    int countPosts(map<string, map<string, string>> string_storage) {
+      string highest_n= "0";
+      for (const auto& pair : string_storage) {
+        const string& n_value = pair.first;
+        if (n_value > highest_n) {
+          highest_n = n_value;
+        }
+      }
+      return stoi(highest_n);
+    }
+
+    int totalUniqueWords(map<string, map<string, string>> string_storage, set<string> &unique_word_set) {
+      int total = 0;
+      for (const auto& outerPair : string_storage) {
+        for (const auto& innerPair : outerPair.second) {
+            total += unique_words(innerPair.second).size();
+            unique_word_set.insert(unique_words(innerPair.second).begin(), unique_words(innerPair.second).end());
+        }
+      }
+      return total;
+    }
+
+    void wordOccurances(map<string, int> &word_occur, map<string, map<string, string>> string_storage) {
+      for (const auto& outerPair : string_storage) {
+        for (const auto& innerPair : outerPair.second) {
+          istringstream stream(innerPair.second);
+          string word;
+          set<string> uniqueWordsInString;
+
+          while (stream >> word) {
+            uniqueWordsInString.insert(word);
+          }
+
+          for (const auto& uniqueWord : uniqueWordsInString) {
+            if (uniqueWordsInString.count(uniqueWord)) {
+                ++word_occur[uniqueWord];
+            }
+          }
+        }
+      }
+    }
+
+    void labelOccurances(map<string, int> &label_occur, map<string, map<string, string>> string_storage) {
+      set<string> uniqueLabelsInString;
+      for (const auto& outerPair : string_storage) {
+        for (const auto& innerPair : outerPair.second) {
+          string word = innerPair.first;
+          uniqueLabelsInString.insert(word);
+        }
+      }
+
+      for (const auto& uniqueLabel : unique_word_set) {
+        if (uniqueLabelsInString.count(uniqueLabel)) {
+          ++label_occur[uniqueLabel];
+        }
+      }
+    }
 };
 
 set<string> unique_words(const string &str) {
